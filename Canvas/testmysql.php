77ff04e3e8106732e16b89fd958e96f500882777
iwtw666 <sxlvlm@outlook.com>
2020-09-14 18:15:54 +1000

    testmysql

--- Canvas/testmysql.php
+++ Canvas/testmysql.php
@@ -9,6 +9,8 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo ("connected to " .$dbname)
 }
 
 mysqli_close($conn);
