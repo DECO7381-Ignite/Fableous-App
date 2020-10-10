0f09a99c83f56620656aea2a3360c632fc5d7aa3
iwtw666 <sxlvlm@outlook.com>
2020-10-10 16:51:09 +1000

    little bugs fix

--- index/Canvas/js/script.js
+++ index/Canvas/js/script.js
@@ -1,7 +1,7 @@
 
-// document.body.addEventListener('touchmove', function (e) {
-//     e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-//   }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
+document.body.addEventListener('touchmove', function (e) {
+    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
 
 // // hide rate and pitch values
 // document.getElementById("rate").style.display = "none";
