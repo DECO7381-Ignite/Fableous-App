3d5b70152592d33ae85ad3f7dddf49bbe233f71a
iwtw666 <sxlvlm@outlook.com>
2020-10-21 19:49:38 +1000

    homepage optimize

--- index/header.php
+++ index/header.php
@@ -53,11 +53,7 @@
         -->
         <?php
         if (!isset($_SESSION['id'])) {
-          echo '<form action="includes/login.inc.php" method="post">
-            <input type="text" name="mailuid" placeholder="E-mail/Username">
-            <input type="password" name="pwd" placeholder="Password">
-            <button type="submit" name="login-submit">Login</button>
-          </form>
+          echo '
           <a href="signup.php" class="header-signup">Signup</a>';
         }
         else if (isset($_SESSION['id'])) {
--- index/index.php
+++ index/index.php
@@ -11,10 +11,25 @@
           -->
           <?php
           if (!isset($_SESSION['id'])) {         
-            echo '<p class="login-status">You are logged out!</p><br><br><br>
+            echo '
+            <div style="float:left; width:60%; margin-right:0">
+            <img src="img/home.jpg" style="width:100%">
+            </div>
+            <div class="header-login" style="text-align:center;float:flex; width:35%; margin-top:9%;">
+            <form action="includes/login.inc.php" method="post">
+            <h2 style="color:#269bb6">F A B E L O U S</h2><br>
+            <p style="color:gray">Welcome back! Please login to your account.</p><br>
+            <input type="text" name="mailuid" placeholder="E-mail/Username"><br>
+            <input type="password" name="pwd" placeholder="Password"><br> 
+            <button type="submit" name="login-submit">Login</button><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
             <p class="login-status">(For developer: user/password - test/test)</p><br>
             <p class="login-status">(For teacher: user/password - teacher/teacher)</p><br>
-            <p class="login-status">(For student: user/password - student/student)</p>';
+            <p class="login-status">(For student: user/password - student/student)</p>
+            </form>
+            
+          </div>
+          ';
           }
           else if (isset($_SESSION['id']) && $_SESSION['id']==2) {
             // content for logged in users
