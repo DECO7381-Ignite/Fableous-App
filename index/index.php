ee61a3760b956a8024f4a038fa6f6933a2f38e80
RaymondCJA <RaymondCJA@gmail.com>
2020-10-22 16:33:43 +1000

    updated comments for login register system

--- index/index.php
+++ index/index.php
@@ -1,14 +1,10 @@
 <?php
-  // To make sure we don't need to create the header section of the website on multiple pages, we instead create the header HTML markup in a separate file which we then attach to the top of every HTML page on our website. In this way if we need to make a small change to our header we just need to do it in one place. This is a VERY cool feature in PHP!
   require "header.php";
 ?>
 
     <main>
       <div class="wrapper-main">
         <section class="section-default">
-          <!--
-          We can choose whether or not to show ANY content on our pages depending on if we are logged in or not. I talk more about SESSION variables in the login.inc.php file!
-          -->
           <?php
           if (!isset($_SESSION['id'])) {         
             echo '
@@ -33,20 +29,11 @@
           }
           else if (isset($_SESSION['id']) && $_SESSION['id']==2) {
             // content for logged in users
-            ////echo '<p class="login-status">You are logged in!</p>';
             header('Location: Canvas/teacher.php');
-            //include('DECO-7381/Canvas/index.php');
-            //readfile('DECO-7381/Canvas/index.php');
-            // above 2 do not render elements properly
           }
           else if (isset($_SESSION['id']) && $_SESSION['id']==3) {
             // content for logged in users
-            ////echo '<p class="login-status">You are logged in!</p>';
             header('Location: Canvas/home.php');
-            // echo '<a href="Canvas/home.html">Click here to access home</a>';
-            //include('DECO-7381/Canvas/index.php');
-            //readfile('DECO-7381/Canvas/index.php');
-            // above 2 do not render elements properly
           }
           ?>
         </section>
@@ -54,6 +41,5 @@
     </main>
 
 <?php
-  // And just like we include the header from a separate file, we do the same with the footer.
 ?>
 
